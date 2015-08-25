bf9eae435fa1bc958d8b6d2e75afba6cb58f6ad7
MadnessFreak <supervivo@icloud.com>
2015-08-25 23:00:42 +0200

    Add visual studio project

---
+++ Code/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die mit einer Assembly verknüpft sind.
+[assembly: AssemblyTitle("IntegrityChecker")]
+[assembly: AssemblyDescription("Provides a tool for data integrity checking.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MadnessFreak")]
+[assembly: AssemblyProduct("IntegrityChecker")]
+[assembly: AssemblyCopyright("Copyright (c) 2015 MadnessFreak")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten. Wenn Sie auf einen Typ in dieser Assembly von 
+// COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("c7bc7dd5-f294-4fca-b32d-ed122b85c151")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.825.*")]
+[assembly: AssemblyFileVersion("0.825.0.0")]
